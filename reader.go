790269a91b16f11f22322b49890e34a69b7c70ea
rpoletaev <losaped@gmail.com>
2016-11-16 03:27:43 +0300

    init

---
+++ reader.go
@@ -0,0 +1,114 @@
+package respio
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"strconv"
+)
+
+//RESPReader allow read RESP
+type RESPReader struct {
+	reader *bufio.Reader
+}
+
+// NewReader creates new RESPReader
+func NewReader(reader io.Reader) *RESPReader {
+	return &RESPReader{
+		reader: bufio.NewReader(reader),
+	}
+}
+
+func (r *RESPReader) getLine() ([]byte, error) {
+	line, _, err := r.reader.ReadLine()
+	if err != nil {
+		return nil, err
+	}
+
+	if len(line) < 2 {
+		return nil, fmt.Errorf("Wrong RESP line")
+	}
+
+	return line, nil
+}
+
+func (r *RESPReader) Read() (interface{}, error) {
+	line, err := r.getLine()
+	fmt.Printf("Readed line is %s\n", string(line))
+	if err != nil {
+		return nil, err
+	}
+
+	switch line[0] {
+	case '-':
+		return errors.New(string(line[1:])), nil
+	case '+':
+		return string(line[1:]), nil
+	case ':':
+		return strconv.ParseInt(string(line[1:]), 10, 64)
+	case '$':
+		return r.parseBulk(line[1:])
+	case '*':
+		fmt.Println("Is a asterisk")
+		length, err := strconv.ParseInt(string(line[1:]), 10, 64)
+		println("length is ", length)
+		if length == -1 && err == nil {
+			return nil, nil
+		}
+
+		response := make([]interface{}, length)
+		for i := range response {
+			println("array iteration ", i)
+			response[i], err = r.Read()
+			fmt.Printf("response %v\n", response[i])
+			if err != nil {
+				return nil, err
+			}
+		}
+		return response, err
+	default:
+		return nil, errors.New("WRONGTYPE")
+	}
+}
+
+//ReadCommand returns incomming command name, parameters or error
+//RESP command format must be array
+func (r *RESPReader) ReadCommand() (string, []interface{}, error) {
+	rawCmd, err := r.Read()
+	if err != nil {
+		return "", nil, fmt.Errorf("Wrong command format: %s", err)
+	}
+	switch rawSlice := rawCmd.(type) {
+	case []interface{}:
+		if len(rawSlice) == 0 {
+			return "", nil, fmt.Errorf("Wrong command format")
+		}
+		if len(rawSlice) == 1 {
+			return string(rawSlice[0].([]byte)), nil, nil
+		}
+
+		return string(rawSlice[0].([]byte)), rawSlice[1:], nil
+	default:
+		return "", nil, fmt.Errorf("Wrong command format. Command must be array")
+	}
+}
+
+func (r *RESPReader) parseBulk(src []byte) (interface{}, error) {
+	length, err := strconv.ParseInt(string(src), 10, 32)
+	if length == -1 && err == nil {
+		return nil, nil
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("Wrong BulkString format: %s", err)
+	}
+
+	bulk := make([]byte, length)
+	_, err = io.ReadFull(r.reader, bulk)
+	if err != nil {
+		return nil, err
+	}
+
+	return bulk, nil
+}
---
+++ writer.go
@@ -0,0 +1,136 @@
+package respio
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"strconv"
+)
+
+type RESPWriter struct {
+	writer *bufio.Writer
+}
+
+func NewWriter(w io.Writer) *RESPWriter {
+	return &RESPWriter{
+		writer: bufio.NewWriter(w),
+	}
+}
+
+func (w *RESPWriter) SendArray(length int64) error {
+	blkLen := []byte{'*'}
+	w.writer.Write(strconv.AppendInt(blkLen, length, 10))
+	_, err := w.writer.WriteString("\r\n")
+	return err
+}
+
+//SendBulkString convert val to Bulk string format
+func (w *RESPWriter) SendBulkString(val string) error {
+	println("sended string is: ", val)
+	blkLen := []byte{'$'}
+	blkLen = strconv.AppendInt(blkLen, int64(len(val)), 10)
+	println("blkLen is ", string(blkLen))
+	w.writer.Write(blkLen)
+	w.writer.WriteString("\r\n")
+	w.writer.WriteString(val)
+	_, err := w.writer.WriteString("\r\n")
+	if err != nil {
+		println("error on sending string: ", err.Error())
+	}
+	return err
+}
+
+//SendBulk convert val to Bulk format and send
+func (w *RESPWriter) SendBulk(val []byte) error {
+	blkLen := []byte{'$'}
+	blkLen = strconv.AppendInt(blkLen, int64(len(val)), 10)
+	w.writer.Write(blkLen)
+	w.writer.WriteString("\r\n")
+	w.writer.Write(val)
+	_, err := w.writer.WriteString("\r\n")
+	return err
+}
+
+//SendRESPInt format and send RESP Int val as :val
+//http://redis.io/topics/protocol#resp-integers
+func (w *RESPWriter) SendRESPInt(val int64) error {
+	bts := []byte{':'}
+	bts = strconv.AppendInt(bts, val, 10)
+	w.writer.Write(bts)
+	_, err := w.writer.WriteString("\r\n")
+	return err
+}
+
+//SendBulkInt format integer value as bulk string to send command to server
+func (w *RESPWriter) SendBulkInt(val int64) error {
+	var valBuf []byte
+	valBuf = strconv.AppendInt(valBuf, val, 10)
+	return w.SendBulk(valBuf)
+}
+
+//SendCmd format cmd and parameters into resp array
+func (w *RESPWriter) SendCmd(cmd string, prs []interface{}) error {
+	bts := []byte{'*'}
+	bts = strconv.AppendInt(bts, int64(len(prs)+1), 10)
+	w.writer.Write(bts)
+	w.writer.WriteString("\r\n")
+	err := w.SendBulkString(cmd)
+
+	fmt.Printf("from WRITER prs is %v\n", prs)
+	if prs == nil {
+		return nil
+	}
+
+	for _, prm := range prs {
+		if err != nil {
+			return err
+		}
+		switch param := prm.(type) {
+		case []byte:
+			err = w.SendBulk(param)
+			break
+		case string:
+			err = w.SendBulkString(param)
+			break
+		case int:
+			err = w.SendBulkInt(int64(param))
+			break
+		case int64:
+			err = w.SendBulkInt(param)
+			break
+		default:
+			err = errors.New("WRONGTYPE")
+			break
+		}
+	}
+	return err
+}
+
+//SendNil send nil value as $-1\r\n
+func (w *RESPWriter) SendNil() error {
+	w.writer.Write([]byte{'$', '-', '1'})
+	_, err := w.writer.WriteString("\r\n")
+	return err
+}
+
+// SendError write string in format -err\r\n
+func (w *RESPWriter) SendError(err string) error {
+	w.writer.Write([]byte{'-'})
+	w.writer.WriteString(err)
+	_, werr := w.writer.WriteString("\r\n")
+	return werr
+}
+
+//SendSimpleString  write string in format +str\r\n
+func (w *RESPWriter) SendSimpleString(str string) error {
+	w.writer.Write([]byte{'+'})
+	w.writer.WriteString(str)
+	_, werr := w.writer.WriteString("\r\n")
+	return werr
+}
+
+//Flush just flush
+func (w *RESPWriter) Flush() error {
+	return w.writer.Flush()
+}
